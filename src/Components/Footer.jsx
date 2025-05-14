7fccd061b5b1aee87da398f3dba18db582509789
Wassefdabbas <wassef.dabbas@gmail.com>
2025-05-14 22:47:22 +0300

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,19 +4,6 @@ import { Link } from "react-router-dom";
 export default function Footer() {
   return (
     <footer className="app-footer">
-      <div className="footer-content">
-        <span>© {new Date().getFullYear()} PIXON</span>
-        <div className="footer-nav">
-          <a
-            href="https://www.themoviedb.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            TMDB
-          </a>
-          <Link to="/">Home</Link>                  {/* client‑side */}
-          <Link to="/favorites">Favorites</Link>    {/* typo fixed */}
-        </div>
         <p className="Owner">Made By Mo_Wassef</p>
       </div>
     </footer>
